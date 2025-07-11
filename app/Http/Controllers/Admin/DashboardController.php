261bfb1dc8fe2eee8fe6141f3394443c4ef7e552
ivan zhivo <vtar4a@gmail.com>
2025-07-11 15:15:51 +0300

    13вфыв

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Car;
+use App\Models\ActivityLog;
+
 class DashboardController extends Controller
 {
-    public function dashboard()
+    public function index()
     {
-        return view('admin.dashboard');
+        $stats = [
+            'usersCount' => User::count(),
+            'postsCount' => Post::count(),
+            'carsCount' => Car::count(),
+            'recentActivities' => ActivityLog::latest()->take(5)->get()
+        ];
+
+        return view('admin.dashboard', $stats);
     }
 }
